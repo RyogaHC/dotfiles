db75664c691e318eb7eeefcfbfdb7f1837b9913d
RyogaHC <frogtuno@gmail.com>
2025-09-22 14:37:16 +0900

    a

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,7 @@
+if vim.g.neovide then
+	vim.g.neovide_scale_factor= 0.7
+end
+
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cursorline = true
@@ -58,12 +62,12 @@ vim.cmd('colorscheme tokyonight-night')
 
 -- Lua
 require('lualine').setup {
-  options = {
-    -- ... your lualine config
-    theme = 'tokyonight'
-    -- theme = 'dracula-nvim'
-    -- ... your lualine config
-  }
+	options = {
+		-- ... your lualine config
+		theme = 'tokyonight'
+		-- theme = 'dracula-nvim'
+		-- ... your lualine config
+	}
 }
 
 require("ibl").setup()
@@ -74,13 +78,13 @@ require("nvim-autopairs").setup({})
 require("nvim-surround").setup({})
 
 vim.fn['skkeleton#config']({
-  globalDictionaries = {
-    "~/.skk/SKK-JISYO.L"
-  }
---  eggLikeNewline = true,
-  -- keepState = true,
-  -- showCandidatesCount = 2,
-  -- registerConvertResult = true,
+	globalDictionaries = {
+		"~/.skk/SKK-JISYO.L"
+	}
+	--  eggLikeNewline = true,
+	-- keepState = true,
+	-- showCandidatesCount = 2,
+	-- registerConvertResult = true,
 })
 -- vim.fn['skkeleton#register_keymap']('input', '/', 'abbrev')
 vim.api.nvim_set_keymap('i', '<C-j>', '<Plug>(skkeleton-enable)', {noremap = true})
@@ -177,33 +181,33 @@ vim.lsp.enable('pylsp')
 
 -- vim.api.nvim_create_autocmd("VimEnter",
 -- {
--- 	callback = function() require("screenkey").toggle() end
--- })
+	-- 	callback = function() require("screenkey").toggle() end
+	-- })
 
-require('gen').setup({
-	model = "gpt-oss", -- The default model to use.
-	quit_map = "q", -- set keymap to close the response window
-	retry_map = "<c-r>", -- set keymap to re-send the current prompt
-	accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
-	host = "localhost", -- The host running the Ollama service.
-	port = "11434", -- The port on which the Ollama service is listening.
-	display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split" or "vertical-split".
-	show_prompt = false, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
-	show_model = false, -- Displays which model you are using at the beginning of your chat session.
-	no_auto_close = false, -- Never closes the window automatically.
-	file = false, -- Write the payload to a temporary file to keep the command short.
-	hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
-	init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
-	-- Function to initialize Ollama
-	command = function(options)
-		local body = {model = options.model, stream = true}
-		return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
-	end,
-	-- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-	-- This can also be a command string.
-	-- The executed command must return a JSON object with { response, context }
-	-- (context property is optional).
-	-- list_models = '<omitted lua function>', -- Retrieves a list of model names
-	result_filetype = "markdown", -- Configure filetype of the result buffer
-	debug = false -- Prints errors and the command which is run.
-})
+	require('gen').setup({
+		model = "gpt-oss", -- The default model to use.
+		quit_map = "q", -- set keymap to close the response window
+		retry_map = "<c-r>", -- set keymap to re-send the current prompt
+		accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
+		host = "localhost", -- The host running the Ollama service.
+		port = "11434", -- The port on which the Ollama service is listening.
+		display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split" or "vertical-split".
+		show_prompt = false, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
+		show_model = false, -- Displays which model you are using at the beginning of your chat session.
+		no_auto_close = false, -- Never closes the window automatically.
+		file = false, -- Write the payload to a temporary file to keep the command short.
+		hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
+		init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
+		-- Function to initialize Ollama
+		command = function(options)
+			local body = {model = options.model, stream = true}
+			return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
+		end,
+		-- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+		-- This can also be a command string.
+		-- The executed command must return a JSON object with { response, context }
+		-- (context property is optional).
+		-- list_models = '<omitted lua function>', -- Retrieves a list of model names
+		result_filetype = "markdown", -- Configure filetype of the result buffer
+		debug = false -- Prints errors and the command which is run.
+	})
