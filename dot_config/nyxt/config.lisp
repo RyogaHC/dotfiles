2e07742ff36147abf1cd1831f15370770cf97974
RyogaHC <frogtuno@gmail.com>
2025-09-28 11:48:11 +0900

    fixed capslock behavior in the i3 config file

--- dot_config/nyxt/config.lisp
+++ dot_config/nyxt/config.lisp
@@ -1,17 +1,28 @@
+;; (defvar *my-search-engines*
+;;   (list
+;;     '("google" "https://google.com/search?q=~a" "https://google.com")))
+
 (define-configuration (input-buffer)
-  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+		      ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
 
 (define-configuration (prompt-buffer)
-  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+		      ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
 
 (define-configuration browser
-  ((theme theme:+dark-theme+)))
+		      ((theme theme:+dark-theme+)))
 
 (define-configuration (web-buffer)
-  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
+		      ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
 
 (define-configuration (input-buffer)
-  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+		      ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
 
 (define-configuration (prompt-buffer)
-  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+		      ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+;; (define-configuration context-buffer
+;; 		      ((search-engines
+;; 			 (append
+;; 			   (mapcar (lambda (engine) (apply 'make-search-engine engine))
+;; 				   *my-search-engines*)
+;; 			   %slot-default%))))
