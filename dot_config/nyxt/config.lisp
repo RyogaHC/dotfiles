28369d9dcb13026ecc6f918d841264d32d76e447
RyogaHC <frogtuno@gmail.com>
2025-09-28 10:33:03 +0900

    a

---
+++ dot_config/nyxt/config.lisp
@@ -0,0 +1,17 @@
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
+
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
