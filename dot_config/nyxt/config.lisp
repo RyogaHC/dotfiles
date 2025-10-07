c55f14313647543a1179f01cd12e8f91fdac440f
RyogaHC <frogtuno@gmail.com>
2025-10-08 08:05:31 +0900

    a

--- dot_config/nyxt/config.lisp
+++ dot_config/nyxt/config.lisp
@@ -1,15 +1,11 @@
-;; (defvar *my-search-engines*
-;;   (list
-;;     '("google" "https://google.com/search?q=~a" "https://google.com")))
-
 (define-configuration (input-buffer)
 		      ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
 
 (define-configuration (prompt-buffer)
 		      ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
 
-(define-configuration browser
-		      ((theme theme:+dark-theme+)))
+;; (define-configuration browser
+;; 		      ((theme theme:+dark-theme+)))
 
 (define-configuration (web-buffer)
 		      ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
@@ -19,10 +15,3 @@
 
 (define-configuration (prompt-buffer)
 		      ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
-
-;; (define-configuration context-buffer
-;; 		      ((search-engines
-;; 			 (append
-;; 			   (mapcar (lambda (engine) (apply 'make-search-engine engine))
-;; 				   *my-search-engines*)
-;; 			   %slot-default%))))
