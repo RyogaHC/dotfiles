4fb156960d89e25136589d37e1929bf693cffbfa
RyogaHC <frogtuno@gmail.com>
2025-10-26 13:57:24 +0900

    updated for i3

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -23,7 +23,7 @@ if vim.g.neovide then
 	-- vim.g.neovide_scale_factor= 0.5
 	vim.g.neovide_scroll_animation_length = 0.1
 	vim.g.neovide_position_animation_length = 0.1
-	vim.g.neovide_opacity = 0.5
+	vim.g.neovide_opacity = 0.7
 	vim.g.neovide_normal_opacity = 0.5
 end
 
